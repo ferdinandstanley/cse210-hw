c5e771cd5ec43cfc6c3e54f17ddd609980c5fe24
ferdinandstanley <145779786+ferdinandstanley@users.noreply.github.com>
2024-09-21 03:31:07 -0600

    Completed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 95)
+        {
+            letter = "A";
+        }
+        else if (percent >= 75)
+        {
+            letter = "B";
+        }
+        else if (percent >= 65)
+        {
+            letter = "C";
+        }
+        else if (percent >= 55)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 75)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Try again next time");
+        }
     }
 }
