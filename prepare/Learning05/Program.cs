9cb1361ab253bf13c5c2ed1a58d0675411dfd3fa
ferdinandstanley <145779786+ferdinandstanley@users.noreply.github.com>
2024-10-21 01:41:25 -0600

    done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        
+        Assignment a1 = new Assignment("Stanley North", "Algebra");
+        Console.WriteLine(a1.GetSummary());
+
+        
+        MathAssignment a2 = new MathAssignment("East Stanley", "Ratio", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Chinua Achiebe", "Our History", "Things Fall Apart");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
